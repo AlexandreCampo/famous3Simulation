e1bd2d5020972fa002503b87afd7747286a431c6
Alexandre Campo <alexandre.campo@gmail.com>
2017-07-04 08:24:53 +0200

    adding electric sense device, testing with afish

---
+++ subCULTron/aFishElectricSense/ControllerAFish.cpp
@@ -0,0 +1,226 @@
+/*----------------------------------------------------------------------------*/
+/*    Copyright (C) 2011-2017 Alexandre Campo                                 */
+/*                                                                            */
+/*    This file is part of FaMouS  (a fast, modular and simple simulator).    */
+/*                                                                            */
+/*    FaMouS is free software: you can redistribute it and/or modify          */
+/*    it under the terms of the GNU General Public License as published by    */
+/*    the Free Software Foundation, either version 3 of the License, or       */
+/*    (at your option) any later version.                                     */
+/*                                                                            */
+/*    FaMouS is distributed in the hope that it will be useful,               */
+/*    but WITHOUT ANY WARRANTY; without even the implied warranty of          */
+/*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           */
+/*    GNU General Public License for more details.                            */
+/*                                                                            */
+/*    You should have received a copy of the GNU General Public License       */
+/*    along with FaMouS.  If not, see <http://www.gnu.org/licenses/>.         */
+/*----------------------------------------------------------------------------*/
+
+#include "ControllerAFish.h"
+
+#include "Simulator.h"
+
+#include <cmath>
+#include <iostream>
+
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
+
+#include <Eigen/Eigen>
+
+extern gsl_rng* rng;
+extern long int rngSeed;
+
+#define EXPLORE 0
+#define TURN 1
+
+using namespace std;
+
+ControllerAFish::ControllerAFish (aFish* fish)
+{
+    this->fish = fish;
+
+    reset();
+}
+
+
+void ControllerAFish::step ()
+{
+    time = object->simulator->time;
+
+    Eigen::VectorXf pola(5);
+    pola << 1, 0, 0, 0, 0;
+    fish->esense->setPolarization (pola);
+    
+    // read e-sense
+    fish->esense->getCurrents();
+    
+    cout << "E-sense current measured : ";    
+    for (int i = 0; i < fish->esense->numElectrodes; i++)
+    {
+	cout << fish->esense->I(i) << " ";
+    }
+    cout << endl;
+    
+    
+    // send a message
+    fish->optical->send(1);
+    
+    // receive messages
+    DeviceOpticalTransceiver::Message msg;
+    while (fish->optical->receive(msg))
+    {
+//	if (dbg) cout << this << " fish received msg " << msg.content << " at time " << time << endl;
+    }
+
+    
+    switch (state)
+    {
+    case EXPLORE : stateExplore(); break;
+    case TURN : stateTurn(); break;
+    }
+}
+
+void ControllerAFish::stateExploreInit ()
+{
+    float rnd = 1.0 - gsl_ran_flat(rng, 0.0, 1.0);
+    exploreDuration = - log (rnd) * exploreMeanDuration;
+
+    exploreStartTime = time;
+    state = EXPLORE;
+
+    // set robot's colour
+    fish->setColor(1, 1, 55.0/254.0);
+}
+
+
+void ControllerAFish::stateExplore ()
+{
+    // if time to change direction -> turn
+    if (time - exploreStartTime > exploreDuration)
+    {
+	// jump to turn state
+	float angle = gsl_rng_uniform(rng) * 2.0 * M_PI - M_PI;    
+	stateTurnInit(EXPLORE, angle);
+
+//	cout << "turning" << endl;
+	return;
+    }
+
+    // inside the state    
+    if (obstacleAvoidance ())
+	return;
+
+    fish->propellerLeft->setSpeed(exploreSpeed);
+    fish->propellerRight->setSpeed(exploreSpeed);
+}
+
+
+void ControllerAFish::stateTurnInit(int previousState, float angle)
+{
+    turnPreviousState = previousState;
+
+    if (angle < 0.0)
+	turnSign = 1.0;
+    else
+	turnSign = -1.0;
+
+    turnDuration = (fabs(angle) / M_PI) / 3.0 / turnSpeed;
+    turnStartTime = time;
+    state = TURN;
+}
+
+void ControllerAFish::stateTurn()
+{
+    // transitions to other states
+    if (time - turnStartTime > turnDuration)
+    {
+	switch (turnPreviousState)
+	{
+	case EXPLORE : stateExploreInit(); return;
+	}
+    }
+
+    // inside the state
+    fish->propellerLeft->setSpeed(turnSpeed * turnSign);
+    fish->propellerRight->setSpeed(-turnSpeed * turnSign);
+}
+
+
+bool ControllerAFish::obstacleAvoidance()
+{    
+    // check if an obstacle is perceived 
+    int obstaclePerceived = 0;
+
+    float pl = fish->rayFrontLU->getValue() + fish->rayFrontLD->getValue() + fish->rayLeft->getValue();
+    float pr = fish->rayFrontRU->getValue() + fish->rayFrontRD->getValue() + fish->rayRight->getValue();
+    pl /= 3.0;
+    pr /= 3.0;   
+
+    // don't take into account down obstacles, as it can be the ground...
+    if (fish->rayFrontLU->hasHit()
+//	|| fish->rayFrontLD->hasHit()
+	|| fish->rayFrontRU->hasHit()
+//	|| fish->rayFrontRD->hasHit()
+	|| fish->rayLeft->hasHit()
+	|| fish->rayRight->hasHit()
+	)
+	obstaclePerceived = 1;	    
+    
+    // no obstacles to avoid, return immediately
+    if (obstaclePerceived == 0)
+	return false;
+    
+    float leftSpeed = 0.0;
+    float rightSpeed = 0.0;
+
+    // proportional break
+    if (pr > obstacleAvoidanceThreshold && pl > obstacleAvoidanceThreshold)
+    {
+	// break symmetry
+	leftSpeed = -breakSpeed * pr / (pr + pl);
+	rightSpeed = -breakSpeed * pl / (pr + pl);	
+    }
+    // turn left
+    if (pr >= pl)
+    {
+	leftSpeed = -obstacleAvoidanceSpeed;
+	rightSpeed = obstacleAvoidanceSpeed;
+    }
+    // turn right
+    else
+    {
+	leftSpeed = obstacleAvoidanceSpeed;
+	rightSpeed = -obstacleAvoidanceSpeed;
+    }
+    
+    // change movement direction
+    fish->propellerLeft->setSpeed(leftSpeed);
+    fish->propellerRight->setSpeed(rightSpeed);
+
+    // advertise obstacle avoidance in progress
+    return true;
+}
+
+
+void ControllerAFish::reset ()
+{
+    // reset time
+    time = 0.0;
+
+    // state working variables
+    exploreDuration = 0.0;
+    exploreStartTime = 0.0;
+
+    turnPreviousState = 0;
+    turnDuration = 0.0;
+    turnStartTime = 0.0;
+    turnSign = 1.0;
+    
+    // start in explore state
+    state = EXPLORE;
+    stateExploreInit();
+}
+
+
--- subCULTron/aFishElectricSense/Experiment.cpp
+++ subCULTron/aFishElectricSense/Experiment.cpp
@@ -27,11 +27,9 @@
 // Objects
 #include "AquariumCircular.h"
 #include "aFish.h"
-#include "aPad.h"
-#include "aMussel.h"
 
 // Controllers
-#include "ControllerAPad.h"
+#include "ControllerAFish.h"
 #include "Experiment.h"
 
 // Utilities
@@ -53,8 +51,7 @@ float calculateWaterVolumeHeight(btVector3 pos, float time)
     float shift = time / M_PI * 2.0;
     float height = 2.0 + amplitude * (sin(pos.getX() * phase + shift) * sin(pos.getY() * phase + shift));    
     
-    return height;
-    
+    return height;    
 }
 
 btVector3 calculateWaterVolumeCurrent(btVector3 pos, float time)
@@ -87,7 +84,7 @@ Experiment::Experiment (Simulator* simulator, bool graphics)
 	render->viewer->getCamera()->setProjectionMatrixAsPerspective(45.0, 1.0, 0.1, 1000); 
 
 	osgGA::SphericalManipulator* manip = dynamic_cast<osgGA::SphericalManipulator*>(render->viewer->getCameraManipulator());
-	manip->setDistance(19);
+	manip->setDistance(7);
 	manip->setCenter(osg::Vec3(0,0,1.0));
 	manip->setElevation(90.0 * M_PI / 180.0);
 	manip->setHeading(0.0 * M_PI / 180.0);
@@ -96,30 +93,38 @@ Experiment::Experiment (Simulator* simulator, bool graphics)
     // add the experiment so that we can step regularly
     simulator->add (this);
     
-    // add aPads
-    for (int i = 0; i < aPadCount; i++)
+    // add aFish
+    for (int i = 0; i < aFishCount; i++)
     {
-	aPad* r = new aPad ();
+	aFish* r = new aFish ();
 	r->registerService(physics);
 	r->registerService(waterVolume);
 	if (render) 
 	{	    
-	    r->setMeshFilename ("../3dmodels/aPad.3ds.(0.025,0.025,0.025).scale");
+	    r->setMeshFilename ("../3dmodels/aFish.3ds.(0.01,0.01,0.01).scale");
 	    r->registerService(render); 
-	}	
-	r->addDevices();	
-	
-	ControllerAPad* c = new ControllerAPad (r);	
+	}
+	r->addDevices();
+//	r->optical->setDrawable(true);
+//	r->optical->setReceiveOmnidirectional(false);
+	r->setDragCoefficients(btVector3( 0.05, 0.3, 0.1), btVector3( 0.05, 0.05, 0.1));
+
+	ControllerAFish* c = new ControllerAFish (r);
 	r->add(c);
 	c->setTimestep(0.1);
-	
-	aPads.push_back(r);
+
+	aFishes.push_back(r);
 	simulator->add(r);   	
 	
 	// position is set in reset
     }
 
-    AquariumCircular* aquarium = new AquariumCircular(aquariumRadius,  10.0, 1.0, 40.0);
+    // create one debug afish
+    ControllerAFish* c = (ControllerAFish*) aFishes[0]->controllers.front();
+//    aFishes[0]->optical->setDrawable(true);
+    c->dbg=1;
+
+    AquariumCircular* aquarium = new AquariumCircular(aquariumRadius,  3.0, 1.0, 40.0);
     aquarium->registerService(physics);
     aquarium->registerService(waterVolume);
     if (render) aquarium->registerService(render);
@@ -135,28 +140,21 @@ Experiment::~Experiment()
 
 void Experiment::reset ()
 {
-    // reset aPad
-    for (unsigned int i = 0; i < aPads.size(); i++)
+    // reset aFish
+    for (unsigned int i = 0; i < aFishes.size(); i++)
     {
-	aPad* r = aPads[i];
+	aFish* r = aFishes[i];
 
-	r->body->setLinearVelocity(btVector3(0,0,0));
-	r->body->setAngularVelocity(btVector3(0,0,0));
-	
-	// reset position
-	float k = (float)(i) / (float) (aPads.size());
-	float distance = (0.7 + (k * 0.2 - 0.1)) * aquariumRadius;
-	float angle = 2.0 * M_PI * k + M_PI/4;;
-	// float distance = gsl_rng_uniform(rng) * 0.8 * aquariumRadius;
-	// float angle = (gsl_rng_uniform(rng) * 2.0 * M_PI - M_PI);
+	// reset aFish position
+	float distance = gsl_rng_uniform(rng) * 0.8 * aquariumRadius;
+	float angle = (gsl_rng_uniform(rng) * 2.0 * M_PI - M_PI);
 	float x = cos(angle) * distance;
 	float y = sin(angle) * distance;
-	float z = 2.0 + r->dimensions[2] / 2.0;		
-	
+	float z = 0.7 + r->dimensions[2] / 2.0;	
+
 	r->setPosition (btVector3(x, y, z));
-	r->setRotation (btQuaternion(btVector3(0, 0, 1), 0));
-	for (auto* b : r->ballasts)
-	    b->setBuoyancyFactor(1);
+	r->setRotation (btQuaternion(btVector3(0, 0, 1), gsl_rng_uniform(rng) * M_PI * 2.0 - M_PI));
+	r->ballast->setBuoyancyFactor(0.0);
     }
 }
 
